93b43339fb5ae6f7e5b8c44328896c86043119d2
Science Advances <aubot@hotmail.com>
2025-06-18 21:40:56 +0800

    Update ChIP-seq.sh

--- ChIP-seq/ChIP-seq.sh
+++ ChIP-seq/ChIP-seq.sh
@@ -13,7 +13,7 @@ mamba activate MetaFlow
 
 
 
-mkdir -p tmp Result/01.clean_data/ Result/02.Alignment tmp Result/03.Peak_Calling/NarrowPeak Result/03.Peak_Calling/BroadPeak Result/04.Visualization Result/05.Annotation Result/10.statistics/fastp_report
+mkdir -p tmp Result/01.clean_data/ Result/02.Alignment tmp Result/03.Peak_Calling/NarrowPeak Result/03.Peak_Calling/BroadPeak Result/04.Visualization Result/05.Annotation Result/10.stat/fastp_report
 
 # Get the list of samples from the meta.tsv file
 mapfile -t samples < <(awk 'NR>1 {print $1}' $META)
@@ -32,3 +32,71 @@ print_sample | parallel -j 16 "bedtools intersect -v -abam tmp/{}.bam -b src/mm1
 macs3 callpeak -g hs --treatment Result/02.Alignment/{}.bam --control Result/02.Alignment/{}.bam --pvalue 0.05 --name {} --format BAMPE --outdir Result/03.Peak_Calling/NarrowPeak
 macs3 callpeak -g hs --treatment Result/02.Alignment/{}.bam --control Result/02.Alignment/{}.bam --broad --broad-cutoff 0.1 --name {} --format BAMPE --outdir Result/03.Peak_Calling/BroadPeak
 
+
+# 
+print_sample | parallel -j 4 bamCoverage -b Result/02.Alignment/{}.bam -o cResult/02.Alignment/{}.bw
+
+
+
+# homer Annotaion
+
+
+# stats
+multiBamSummary bins \
+    --bamfiles $(printf "Result/02.Alignment/*bam " "${samples[@]}")  \
+    --minMappingQuality 30 \
+    --labels ${samples[@]} \
+    -out Result/10.stat/CoverageSummary.npz \
+    --outRawCounts Result/10.stat/CoverageSummary.xls
+    # --region 19 \ # limiting the binning of the genome to chromosome 19
+
+# Calculate FRiP score https://deeptools.readthedocs.io/en/develop/content/example_api_tutorial.html#computing-the-frip-score
+# 一个高质量的文库其FRiP score值应该大于0.03，最低也要大于0.02
+import deeptools
+import pysam
+bed_file = "Result/03.Peak_Calling/NarrowPeak/{}_peaks.narrowPeak"
+bam_file = "Result/02.Alignment/{}.bam"
+cr = deeptools.countReadsPerBin.CountReadsPerBin(bam_file, bedFile=bed_file, numberOfProcessors=10)
+reads_at_peaks = cr.run()
+total = reads_at_peaks.sum(axis=0)
+bam = pysam.AlignmentFile(bam_file)
+frip = float(total) / bam.mapped
+pd.DataFrame(dict(Sample=samples,FRiP=frips)).to_excel("Result/10.stat/FRiP_score.xlsx",index=False)
+
+# Correlation 
+plotCorrelation \
+-in Result/10.stat/CoverageSummary.npz \
+--corMethod pearson --skipZeros \
+--plotTitle "Pearson Correlation of Average Scores Per Transcript" \
+--whatToPlot scatterplot \
+-o Result/10.stat/scatter_Pearson_Correlation.pdf   \
+--outFileCorMatrix Result/10.stat/scatter_Pearson_Correlation.xls
+
+plotCorrelation \
+-in Result/10.stat/CoverageSummary.npz \
+--corMethod pearson --skipZeros \
+--plotTitle "Pearson Correlation of Average Scores Per Transcript" \
+--whatToPlot heatmap \
+-o Result/10.stat/heatmap_Pearson_Correlation.pdf   \
+--outFileCorMatrix Result/10.stat/heatmap_Pearson_Correlation.xls
+
+plotPCA -in Result/10.stat/CoverageSummary.npz \
+-o Result/10.stat/PCA_readCounts.pdf \
+-T "PCA of read counts"
+
+
+idr \
+--samples $(printf "Result/03.Peak_Calling/NarrowPeak/%s_peaks.narrowPeak","${samples[@]}") \
+--input-file-type narrowPeak \
+--rank p.value \
+--output-file IDR_narrowPeak \
+--plot \
+--log-output-file IDR_narrowPeak.log
+
+idr \
+--samples $(printf "Result/03.Peak_Calling/NarrowPeak/%s_peaks.broadPeak","${samples[@]}") \
+--input-file-type broadPeak \
+--rank p.value \
+--output-file IDR_broadPeak \
+--plot \
+--log-output-file IDR_broadPeak.log
